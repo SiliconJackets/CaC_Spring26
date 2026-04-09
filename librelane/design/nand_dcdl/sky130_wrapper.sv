9375e023d0f386395c3bf4e8a480b69f5c93c45f
Oliver Lee <xli3086@gatech.edu>
2026-04-08 23:37:08 -0400

    Updated config and sky130 wrapper for nand_dcdl for consistency

--- librelane/design/nand_dcdl/sky130_wrapper.sv
+++ librelane/design/nand_dcdl/sky130_wrapper.sv
@@ -1,12 +1,12 @@
 //**************************************************************************
 // Author: Oliver Lee
-// Description: Wrapper for SKY130 PDK NAND and INV
+// Description: Wrapper for SKY130 PDK Cells
 //**************************************************************************
 
 module nand2 (
-    input logic a,
-    input logic b,
-    output logic out
+    input   logic a,
+    input   logic b,
+    output  logic out
 );
     (* dont_touch = "true" *) sky130_fd_sc_hd__nand2_1 core_nand (
         .A(a), .B(b), .Y(out)
@@ -14,10 +14,21 @@ module nand2 (
 endmodule
 
 module inverter (
-    input logic in,
-    output logic out
+    input   logic in,
+    output  logic out
 );
     (* dont_touch = "true" *) sky130_fd_sc_hd__inv_2 core_inv (
         .A(in), .Y(out)
+    );
+endmodule
+
+module mux (
+    input   logic x0,
+    input   logic x1,
+    input   logic s,
+    output  logic Y
+);
+    (* dont_touch = "true" *) sky130_fd_sc_hd__mux2_1 core_mux (
+        .A0(x0), .A1(x1), .S(s), .X(Y)
     );
 endmodule
