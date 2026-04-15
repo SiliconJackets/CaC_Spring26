5831f18e67cd83b19c4c2c83ff93ca156d13ce34
zhengyinlee <zlee63@gatech.edu>
2026-04-15 00:06:23 -0400

    lint changes

--- librelane/design/phase_detector_syn_edge/phase_detector_syn_edge/phase_detector_syn_edge.sv
+++ librelane/design/phase_detector_syn_edge/phase_detector_syn_edge/phase_detector_syn_edge.sv
@@ -1,28 +1,10 @@
 `timescale 1ps/1ps
 
-/*
-|=======================================================================
-| Module      : edge_order_phase_detector
-| Author      : Mythri Muralikannan
-| Description : First-edge-wins binary phase detector
-|
-| Function:
-|   Determines which clock edge arrives first and holds that decision
-|   until the opposite clock edge arrives, at which point the outputs
-|   are cleared.
-|
-| Output meaning:
-|   up   = 1 -> reference clock leads feedback clock -> speed up loop
-|   down = 1 -> feedback clock leads reference clock -> slow down loop
-|
-| Notes:
-|   - Fully synthesizable
-|   - Simpler than a PFD, but less robust for close/simultaneous edges
-| 
-| Issues:
-| - Case 3 & 5: When clk_out leads clk_in, the detector still asserts UP because the "first-edge-wins" logic lets clk_in set UP before DOWN can capture the earlier feedback edge.
-| - Case 1 & 6: Aligned edges occasionally trigger both signals (UP=1, DOWN=1) due to simultaneous events on the two clocks, showing ambiguity in edge ordering without a proper reset/arbiter.|=======================================================================
-|*/
+//**************************************************************************
+// Module      : edge_order_phase_detector
+// Author      : Mythri Muralikannan
+// Description : First-edge-wins binary phase detector
+//**************************************************************************
 
 module phase_detector (
     input  wire clk_in,
